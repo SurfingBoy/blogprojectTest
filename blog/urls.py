38ca14530132c20a2d6fadf02d9d35d93eb946a3
SurfingBoy <SurfingBoy@users.noreply.github.com>
2018-05-17 15:16:59 +0800

    将view改为类视图

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
 # -*- coding:utf-8 -*-
 from django.urls import path,re_path
 from django.conf.urls import url
+from django.conf.urls import url
 from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    re_path('^$',views.index,name='index'),
+    #re_path('^$',views.index,name='index'),
+    re_path('^$',views.IndexView.as_view(),name='index'),
     re_path('^blog/(?P<pk>[0-9]+)/$',views.detail,name='detail'),
     re_path('^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',views.archives,name='archives'),
-    re_path('^category/(?P<pk>[0-9]+)/$',views.category,name='category'),
+    #re_path('^category/(?P<pk>[0-9]+)/$',views.category,name='category'),
+    re_path('category/(?P<pk>[0-9]+)/$',views.CategoryView.as_view(),name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,21 @@ from django.http import HttpResponse
 from blog.models import Blog,Category,Tag
 from comment.forms import CommentForm
 import markdown
+from django.views.generic import ListView,DetailView
+
 # Create your views here.
 
+#首页
 def index(request):
     blog_list = Blog.objects.all().order_by('-create_time')
     return render(request,'index.html',context={'blog_list':blog_list})
 
+class IndexView(ListView):
+    model = Blog
+    template_name = 'index.html'
+    context_object_name = 'blog_list'
+
+#详情
 def detail(request,pk):
     blog = get_object_or_404(Blog,pk=pk)
     #阅读量+1
@@ -32,12 +41,55 @@ def detail(request,pk):
 
     return render(request,'detail.html',context=context)
 
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'detail.html'
+    context_object_name = 'blog'
+
+    def get(self,request,*args,**kwargs):
+        response = super(BlogDetailView,self).get(request,*args,**kwargs)
+        self.object.increase_views()
+        return response
+    def get_object(self, queryset=None):
+        blog = super(BlogDetailView,self).get_object(queryset=None)
+        blog.body = markdown.Markdown(blog.body,
+                                      extensions=[
+                                          'markdown.extensions.extra',
+                                          'markdown.extensions.codehilite',  # 语法高亮
+                                          'markdown.extensions.toc',  # 自动生成目录
+                                      ])
+        return blog
+    def get_context_data(self, **kwargs):
+        context = super(BlogDetailView,self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form':form,
+            'comment_list':comment_list
+        })
+        return context
+
+#日期归类
 def archives(request,year,month):
     blog_list = Blog.objects.filter(create_time__year=year,
                                     create_time__month=month).order_by('-create_time')
     return render(request,'index.html',context={'blog_list':blog_list})
 
+class ArchivesView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchivesView,self).get_queryset().filter(create_time__year=year,
+                                                              create_time__month=month)
+
+#分类
 def category(request,pk):
     cate = get_object_or_404(Category,pk=pk)
     blog_list = Blog.objects.filter(category=cate).order_by('-create_time')
     return render(request,'index.html',context={'blog_list':blog_list})
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category,pk=self.kwargs.get('pk'))
+        return super(CategoryView,self).get_queryset().filter(category=cate)
+
