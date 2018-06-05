ba105c03c5eab8161934c5acd04f7cc4465ab5f7
SurfingBoy <SurfingBoy@users.noreply.github.com>
2018-06-05 17:10:46 +0800

    Update settings.py
    更换数据库

--- blogprojectTest/settings.py
+++ blogprojectTest/settings.py
@@ -98,17 +98,17 @@ WSGI_APPLICATION = 'blogprojectTest.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'blogTest',
-            'USER': 'root',
-            'PASSWORD': '123456',
-            'PORT': 3306
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+#     'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'blogTest',
+#             'USER': 'root',
+#             'PASSWORD': '123456',
+#             'PORT': 3306
+#         }
 }
 
 
